10fc85ef7359397af52b31933a56a5eb42e9d190
starquell <ihayduchenko@gmail.com>
2023-04-25 23:52:44 +0300

    dockerfile

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello world!')
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import express, * as e from 'express'
+import bodyParser from 'body-parser';
+
+const port = process.env.PORT
+const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/greeting', (req, res) => {
+    res.send({greeting: 'Hello World!'})
+});
+
+app.listen(port, () => {
+    console.log(`⚡️ Chipsnote backend is listening on port ${port}`);
+});
