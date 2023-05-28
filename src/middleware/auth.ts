12eae8bdd504915f4d4da55e30f5908f15824438
starquell <ihayduchenko@gmail.com>
2023-05-28 21:20:28 +0300

    aud fixed

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,7 +2,12 @@ import { auth } from 'express-oauth2-jwt-bearer'
 import * as e from 'express'
 import { ApiError } from '../utils/api-error';
 
-export const jwtCheck = auth();
+export const jwtCheck = auth({
+    audience: [    /// @todo investigate how to get normal audience on web side
+        "r75VwjagN2oAW9nBtGeAMiwsBThj2Y2A",   /// web app client id
+        "https://chipsnote-main.up.railway.app"   /// id of this service
+    ]
+});
 
 export function extractUserData(req: e.Request, res: e.Response, next: e.NextFunction) {
     if (req.auth === undefined) {
